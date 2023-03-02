bde6b9fa16c11cb5af3499f45a253531ccdeefb8
mugeshp87 <mugeshp087@gmail.com>
2023-03-02 17:44:30 +0530

    push

--- file.js
+++ file.js
@@ -1,7 +1,31 @@
+// const filesystem=require('fs');
+// try{
+//     const stats=filesystem.statSync('./hi.txt')
+//     console.log('eror ocuring')
+// }
+// catch(err)
+// {
+// console.log(err)
+// }
 const fs = require("fs");
 
-fs.stat("test.txt", (err, stats) => {
+fs.stat("/Users/joe/test.txt", (err, stats) => {
   if (err) {
     console.error(err);
+    return;
   }
+
+  stats.isFile(); // true
+  stats.isDirectory(); // false
+  stats.isSymbolicLink(); // false
+  stats.size; // 1024000 //= 1MB
 });
+const fs = require("fs");
+
+const pathsToCheck = ["./fileCheckForExistence"];
+
+for (let i = 0; i < pathsToCheck.length; i++) {
+  fs.stat(pathsToCheck[i], function (err, stats) {
+    console.log(stats.isDirectory());
+  });
+}
