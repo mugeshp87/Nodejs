e19ab0c9376492af41e1b8b7326b2fe752a59afc
mugeshp87 <mugeshp087@gmail.com>
2023-03-07 18:17:46 +0530

    push

--- file.js
+++ file.js
@@ -29,3 +29,12 @@ for (let i = 0; i < pathsToCheck.length; i++) {
     console.log(stats.isDirectory());
   });
 }
+const fs = require('fs');
+const util = require('util');
+
+async function example() {
+  const open = util.promisify(fs.open);
+  const fd = await open('/Users/joe/test.txt', 'r');
+}
+example();
+
